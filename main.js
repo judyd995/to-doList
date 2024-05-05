ae7e4ebd3b1e251cfeb0582ac7e29bd91fa4f34c
Judy Darwish <judydarwish3@gmail.com>
2024-05-05 18:33:05 +0300

    js file

---
+++ main.js
@@ -0,0 +1,28 @@
+const inputBox = document.getElementById("input-button");
+const list = document.getElementById("List-container");
+
+function addList(){
+    if(inputBox.value === ''){
+        alert ("you must enter some text");
+    }
+    else {
+     let li = document.createElement("li");
+     li.innerHTML = inputBox.value;
+     list.appendChild(li);
+     let span = document.createElement("span");
+     span.innerHTML="\u00d7";
+     li.appendChild(span);
+    }
+    inputBox.value ="";
+    
+
+}
+
+list.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+            e.target.parentElement.remove();
+        }
+    },false);
